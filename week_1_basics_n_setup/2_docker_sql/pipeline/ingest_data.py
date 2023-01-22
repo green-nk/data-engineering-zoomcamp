1dc7d70a228eb7d37d7293c842a0b2575ca1bc66
green-nk <green.nk21@gmail.com>
2023-01-22 19:59:40 +0700

    build up pipeline script

---
+++ week_1_basics_n_setup/2_docker_sql/pipeline/ingest_data.py
@@ -0,0 +1,35 @@
+import argparse
+from utlis import load_data, connect_db, ingest_data
+
+
+def main(config):
+    """
+    Ingest data from source into Postgresql database.
+    """
+    # Load data from source
+    source_file = load_data(config)
+
+    # Connect to Postgresql database
+    engine = connect_db(config)
+
+    # Ingest data into database
+    ingest_data(source_file, engine, config)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Ingest data from source to database")
+
+    # Source args
+    parser.add_argument("-s", "--source", dest="source", required=True, help="Link to source data")
+    parser.add_argument("-c", "--chucksize", dest="chucksize", default=100000, help="Number of records to be loaded each time")
+
+    # Postgresql connection args
+    parser.add_argument("--host", dest="hostname", required=True, help="Postgresql host name")
+    parser.add_argument("-p", "--port", dest="port", required=True, help="Port to connect to Postgresql")
+    parser.add_argument("-u", "--user", dest="username", required=True, help="Username for Postgresql")
+    parser.add_argument("-d", "--database", dest="database", required=True, help="Database to connect to ")
+    parser.add_argument("-t", "--table", dest="table", required=True, help="Table to save the results")
+
+    args = parser.parse_args()
+    config = args.__dict__
+    main(config)
---
+++ week_1_basics_n_setup/2_docker_sql/pipeline/utlis.py
@@ -0,0 +1,70 @@
+import os
+import pandas as pd
+from getpass import getpass
+from sqlalchemy import create_engine
+from time import time
+
+
+def load_data(config):
+    """
+    Load data directly from the given source.
+    """
+    source = config["source"]
+    
+    # Keep source file extension for pandas to load it correctly
+    source_file = "source.csv.gz" if source.endswith(".csv.gz") else "../data/raw/csv/source.csv"
+
+    os.system(f"wget {source} -O {source_file}")
+    print("...Successfully loaded data from source")
+
+    return source_file
+
+
+def connect_db(config):
+    """
+    Build up connection string to create an database engine.
+    """
+    print("Connecting to database...")
+    password = getpass()
+
+    username = config["username"]
+    hostname = config["hostname"]
+    port = config["port"]
+    database = config["database"]
+
+    connection_string = f"postgresql://{username}:{password}@{hostname}:{port}/{database}"
+    engine = create_engine(connection_string)
+
+    # Test connection
+    try:
+        engine.connect()
+        print("...Successfully connected to database")
+    except Exception as e:
+        print(e)
+        
+    return engine
+
+
+def ingest_data(source_file, engine, config):
+    """
+    From raw file ingested into the database.
+    """
+    # Load source data saved on disk onto memory
+    chucksize = config["chucksize"]
+    df_iter = pd.read_csv(source_file, parse_dates=["tpep_pickup_datetime", "tpep_dropoff_datetime"], iterator=True, chunksize=chucksize)
+
+    # Insert each chuck of data into database
+    table = config["table"]
+    batch = 0
+    
+    for df_chuck in df_iter:
+        t_start = time()
+        df_chuck.to_sql(index=False, name=table, con=engine, if_exists="append")
+        t_end = time()
+
+        time_used = t_end - t_start
+        print(f"\tSuccessfully inserted batch No.{batch}, took {time_used:.3f} seconds")
+
+        batch += 1
+
+    print("...Finished ingesting data into the database")
