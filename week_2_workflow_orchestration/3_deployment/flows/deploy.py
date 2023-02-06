edc2ea404f44b1936ec9b54137ffd5466ab9a2ad
green-nk <green.nk21@gmail.com>
2023-02-06 21:59:54 +0700

    build deployment file to setup github storage block to be flow code storage

--- week_2_workflow_orchestration/3_deployment/flows/deploy.py
+++ week_2_workflow_orchestration/3_deployment/flows/deploy.py
@@ -1,14 +1,16 @@
 from prefect.deployments import Deployment
-from prefect.infrastructure.docker import DockerContainer
+from prefect.filesystems import GitHub 
 from flow import main
 
 
-docker_block = DockerContainer.load("docker-ny-taxi")
+storage = GitHub.load("github-ny-taxi")
 
 deployment = Deployment.build_from_flow(
     flow=main, 
-    name="Yellow NY Taxi Data Ingestion on Docker", 
-    infrastructure=docker_block
+    name="Green NY Taxi Data Dumping via GitHub", 
+    parameters={"config": {"taxi-color": "green", "years": [2020], "months": [11], "data-lake": "gcs-data-lake", "credentials": "gcs-data-lake-creds", "project-id": "dtc-de-375601", "dataset": "trips_data_all", "table": "rides"}}, 
+    storage=storage, 
+    entrypoint="week_2_workflow_orchestration/3_deployment/flows/flow.py:main"
 )
 
 if __name__ == "__main__":
